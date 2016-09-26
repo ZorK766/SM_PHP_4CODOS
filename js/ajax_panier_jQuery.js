bdcd804a3d1c2af2c9ea13e515c7113cd61badf6
Stagiaires <Stagiaires@192.168.172.249>
2016-09-26 13:51:05 +0200

    Creation de la panier.ajax.php et modif de ajax_panier_jQuery

--- js/ajax_panier_jQuery.js
+++ js/ajax_panier_jQuery.js
@@ -1,16 +1,28 @@
 
 jQuery(document).ready(function () {
     
+    //detection du clic sur le formulaire add_cart
     $(".add_cart").click(function(){
+        
+        //recupération des id qte et id_articles
+        
+        //récupère l'article cliqué
         var $id = $(this);
+        //récupère son id article
         var id_article = $id.attr("id");
         
-        console.log(id_article);
+        // recupère la valeur de l'id qte concatené suivant la valeur de l'id article
+        var qte = $("qte_"+id_article).val();
+        
+        console.log(id_article);  
+        console.log(qte);
+        
+        //
         $.post(
             'panier.ajax.php', // Le fichier cible côté serveur.
             {
-                qte : $("#ajouter_panier").val(),// Nous supposons que ce formulaire existe dans le DOM.
-                codeA : $("#contenu").val()
+                qte : $("qte").val(),// Nous supposons que ce formulaire existe dans le DOM.
+                codeA : $("id_article").val()
             },
             'nom_fonction_retour', // Nous renseignons uniquement le nom de la fonction de retour.
             'text' // Format des données reçues.
--- panier.ajax.php
+++ panier.ajax.php
@@ -2,6 +2,11 @@
 
 require_once 'inc/class/config.php';
 
+if(isset($_POST['id_article']))
+{
+    echo 'ok';
+}
+
 $panier_manager = new CommandeManager();
 
 $panier_manager->add();
